a4339b93e88eada08b2b5b6d31ab074c6daf34dc
dhantha <dag332@drexel.edu>
2015-03-06 14:02:38 -0500

    added to gui

--- assignment3.m
+++ assignment3.m
@@ -69,7 +69,7 @@ handles.magfil_dy = zeros(handles.bufferlength,1);
 handles.magfil_dz = zeros(handles.bufferlength,1);
 
 %handles.magfiltdata = zeros(handles.bufferlength,1);
-
+G = SpriteKit.Game.instance('Title','Galaxy','Size',[800 400]);
 % Update handles structure
 guidata(hObject, handles);
 
@@ -141,45 +141,152 @@ function timer(hObject,eventdata,handles)
 % This function draws and update the plot
 
 %contents{get(hObject,'Value')}
+if get(handles.start,'UserData')
+    
+    G = SpriteKit.Game.instance('Title','Galaxy','Size',[800 400]);
+    bkg = SpriteKit.Background('img/galaxy.png');
+    bkg.Scale = 1.8;
+    addBorders(G);
 
-%G = SpriteKit.Game.instance('Title','Interactive Demo','Size',[600 320]);
-bkg = SpriteKit.Background('worldtopo.png');
-bkg.Scale = 1.8;
-axes(handles.axes1)
+    scale_1 = 1.2;
+    scale_2 = 0.4;
+
+    % spaceship 
+    s = SpriteKit.Sprite('ship');
+    s.initState('roaming','img/space.png',true);
+    s.initState('explosion','img/explosion.png',true);
+    s.Scale = scale_1;
+    s.Location = [50 300];
+    s.State = 'roaming';
+
+    % meteorite sprites
+
+    a = SpriteKit.Sprite('a');
+    a.initState('on','img/meteorite.png',true);
+    a.initState('explosion','img/explosion.png',true);
+    a.Location = [randi([25 G.Size(1)-25]) randi([25 G.Size(2)-25])];
+    a.Scale = scale_1;
+    a.State = 'on';
+
+    b = SpriteKit.Sprite('b');
+    b.initState('on','img/meteorite.png',true);
+    b.initState('explosion','img/explosion.png',true);
+    b.Location = [randi([25 G.Size(1)-25]) randi([25 G.Size(2)-25])];
+    b.Scale = scale_1;
+    b.State = 'on';
+
+    c = SpriteKit.Sprite('c');
+    c.initState('on','img/meteorite.png',true);
+    c.initState('explosion','img/explosion.png',true);
+    c.Location = [randi([25 G.Size(1)-25]) randi([25 G.Size(2)-25])];
+    c.Scale = scale_1;
+    c.State = 'on';
+
+    d = SpriteKit.Sprite('d');
+    d.initState('on','img/meteorite.png',true);
+    d.initState('explosion','img/explosion.png',true);
+    d.Location = [randi([25 G.Size(1)-25]) randi([25 G.Size(2)-25])];
+    d.Scale = scale_1;
+    d.State = 'on';
+
+    e = SpriteKit.Sprite('e');
+    e.initState('on','img/meteorite.png',true);
+    e.initState('explosion','img/explosion.png',true);
+    e.Location = [randi([25 G.Size(1)-25]) randi([25 G.Size(2)-25])];
+    e.Scale = scale_1;
+    e.State = 'on';
+
+
+    addprop(s,'accel');
+    s.accel = [0 0];
+    
+else
+    % spriteKit.delete(G);
+    
+end
+
 % Run while the button string reads 'Stop'
 
-while (get(handles.start,'UserData'))
+    while (get(handles.start,'UserData'))
 
- % Read accel data
-%  [gx gy gz]= readAcc(handles.accelerometer,handles.calco);
-%  % Calculate magnitude from raw data
-%  rawmag = sqrt(gx^2+gy^2+gz^2);
+    % Read accel data
+    %  [gx gy gz]= readAcc(handles.accelerometer,handles.calco);
+    %  % Calculate magnitude from raw data
+    %  rawmag = sqrt(gx^2+gy^2+gz^2);
 
-%  
-%  % update the vectors with raw data
-%  handles.magraw_dx = [handles.magraw_dx(2:end);gx];
-%  handles.magraw_dy = [handles.magraw_dy(2:end);gy];
-%  handles.magraw_dz = [handles.magraw_dz(2:end);gz];
-%  
-%  % setup the filters  
-%  filtmag_dx = (1 - handles.alpha)*handles.magfil_dx(end) + handles.alpha*rawmag;
-%  filtmag_dy = (1 - handles.alpha)*handles.magfil_dy(end) + handles.alpha*rawmag;
-%  filtmag_dz = (1 - handles.alpha)*handles.magfil_dz(end) + handles.alpha*rawmag;
-%      
-%  % Calculate filtered magnitude
-%  handles.magfil_dx = [handles.magfil_dx(2:end); filtmag_dx];
-%  handles.magfil_dy = [handles.magfil_dy(2:end); filtmag_dy];
-%  handles.magfil_dz = [handles.magfil_dz(2:end); filtmag_dz];
-% 
-%  % Put new filtered data on end of vector and shift old data up 1 spot
-%  plot(handles.index,handles.magfil_dx,'b'); %,handles.index,handles.magfil_dy,'k');
-%  % Plot raw and filtered data
-% 
-%  %legend('raw data','filtered data');
-%  axis([0 handles.bufferlength -1 10]);
- % Set axis limits
- 
-  bkg.scroll('right',1);
- drawnow % Force redraw of figure
- guidata(hObject, handles);
-end
+    %  
+    %  % update the vectors with raw data
+    %  handles.magraw_dx = [handles.magraw_dx(2:end);gx];
+    %  handles.magraw_dy = [handles.magraw_dy(2:end);gy];
+    %  handles.magraw_dz = [handles.magraw_dz(2:end);gz];
+    %  
+    %  % setup the filters  
+    %  filtmag_dx = (1 - handles.alpha)*handles.magfil_dx(end) + handles.alpha*rawmag;
+    %  filtmag_dy = (1 - handles.alpha)*handles.magfil_dy(end) + handles.alpha*rawmag;
+    %  filtmag_dz = (1 - handles.alpha)*handles.magfil_dz(end) + handles.alpha*rawmag;
+    %      
+    %  % Calculate filtered magnitude
+    %  handles.magfil_dx = [handles.magfil_dx(2:end); filtmag_dx];
+    %  handles.magfil_dy = [handles.magfil_dy(2:end); filtmag_dy];
+    %  handles.magfil_dz = [handles.magfil_dz(2:end); filtmag_dz];
+    % 
+    %  % Put new filtered data on end of vector and shift old data up 1 spot
+    %  plot(handles.index,handles.magfil_dx,'b'); %,handles.index,handles.magfil_dy,'k');
+    %  % Plot raw and filtered data
+    % 
+    %  %legend('raw data','filtered data');
+    %  axis([0 handles.bufferlength -1 10]);
+     % Set axis limits
+     
+     %% game controlls
+     
+         
+     s.accel = s.accel*0.97;
+     L = s.Location;
+     L = L + s.accel;
+     s.Location = L;
+
+     a.Angle = a.Angle-1;
+     b.Angle = b.Angle-1;
+     c.Angle = c.Angle-1;
+     d.Angle = d.Angle-1;
+     e.Angle = e.Angle-1;
+
+     bkg.scroll('right',1);
+
+     a.Location(1) = a.Location(1)-1;
+     b.Location(1) = b.Location(1)-1;
+     c.Location(1) = c.Location(1)-1;
+     d.Location(1) = d.Location(1)-1;
+     e.Location(1) = e.Location(1)-1;
+     
+     if a.Location(1) < 0
+        a.Location = [randi([25 G.Size(2)-35])+650 randi([G.Size(1)-100])];
+        a.State = 'on';
+        a.Scale = scale_1;
+    elseif b.Location(1) < 0
+        b.Location = [randi([25 G.Size(2)-35])+650 randi([G.Size(1)-100])];           
+        b.State = 'on';
+        b.Scale = scale_1;
+    else
+    end
+        
+    if c.Location(1) < 0                
+        c.Location = [randi([25 G.Size(2)-35])+650 randi([G.Size(1)-100])];
+        c.State = 'on';
+        c.Scale = scale_1;
+    elseif d.Location(1) < 0
+        d.Location = [randi([25 G.Size(2)-35])+650 randi([G.Size(1)-100])];
+        d.State = 'on';
+        d.Scale = scale_1;
+    else            
+    end
+    if e.Location(1) < 0
+        e.Location = [randi([25 G.Size(2)-35])+650 randi([G.Size(1)-100])];
+        e.State = 'on';
+        e.Scale = scale_1;
+     end
+     
+     drawnow % Force redraw of figure
+     guidata(hObject, handles);
+    end
